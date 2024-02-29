acf042798f5a9155e755b47b71a8791f7792d2fc
DanielKibatullin <dkibatullin40@stuy.edu>
2024-02-29 14:26:07 -0500

    skeletons

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({
+    data() {
+        return {
+        };
+    },
+    computed: {
+        },
+    },
+    watch:  {
+        },
+    },
+    methods: {
+    },
+}
+}).mount('#app')
