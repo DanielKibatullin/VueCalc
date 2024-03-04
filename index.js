b1c1aa793fb683846b1fba339b5c2eef20b22383
DanielKibatullin <dkibatullin40@stuy.edu>
2024-03-04 14:51:02 -0500

    yep

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
     data() {
         return {
+            equation: "",
         };
     },
     computed: {
@@ -10,8 +11,36 @@ createApp({
     watch:  {
     },
     methods: {
-        del(){
-            console.log("hi");
+        num(x){
+            this.equation += x;
+            console.log(this.equation);
         },
+        operator(operator){
+            this.equation += operator;
+            console.log(this.equation);
+        },
+        del(){
+            console.log(this.equation.length)
+            if ( this.equation.substring(this.equation.length-3) == 'mod')
+                this.equation = this.equation.substring(0,this.equation.length-3)
+            else
+                this.equation = this.equation.substring(0,this.equation.length-1)
+        },
+        mod(){
+            this.equation += 'mod';
+        },
+        pie(){
+            this.equation += 'π';
+        },
+        sqrt(){
+            this.equation += '√';
+        },
+        pow2(){
+            this.equation += '²';
+        },
+        compute(){
+            
+        },
+
     },
 }).mount('#app');
