e92a0e86a502d8bb6f6983f7326364daf0c74e37
DanielKibatullin <dkibatullin40@stuy.edu>
2024-03-06 14:07:57 -0500

    work

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ createApp({
     data() {
         return {
             equation: "",
+            answer: null,
+            substitute : "",
         };
     },
     computed: {
@@ -13,14 +15,13 @@ createApp({
     methods: {
         num(x){
             this.equation += x;
-            console.log(this.equation);
+
         },
         operator(operator){
             this.equation += operator;
-            console.log(this.equation);
+
         },
         del(){
-            console.log(this.equation.length)
             if ( this.equation.substring(this.equation.length-3) == 'mod')
                 this.equation = this.equation.substring(0,this.equation.length-3)
             else
@@ -39,7 +40,35 @@ createApp({
             this.equation += '²';
         },
         compute(){
-            
+            for(let i = 0; i < this.equation.length; i++){
+                if ( this.equation.substring(i,i + 3) == "mod"){
+                    this.substitute = this.equation.substring(0, i ) + "%" + this.equation.substring(i+3)
+                    this.answer = eval(this.substitute)
+                    this.equation = ""
+                    return null;
+                }
+                if ( this.equation.substring(i,i + 1) == "%"){
+                    this.substitute = this.equation.substring(0, i )/100
+                    this.answer = eval(this.substitute)
+                    this.equation = ""
+                    return null;
+                }
+                if ( this.equation.substring(i,i + 1) == "π"){
+                    this.substitute = this.equation.substring(0, i ) + "3.14159265358979323846264338327950288419716939937510" + this.equation.substring(i+1)
+                    this.answer = eval(this.substitute)
+                    this.equation = ""
+                    return null;
+                }
+                
+            }
+            try{
+                this.answer = eval(this.equation)
+                
+            }
+            catch{
+                alert("that didnt work xd")
+            }
+            this.equation = ""
         },
 
     },
