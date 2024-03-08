1939351795f190f025c6d52d168830245e244180
DanielKibatullin <dkibatullin40@stuy.edu>
2024-03-07 20:36:33 -0500

    uhhhh

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 createApp({
-    data() {
-        return {
-            equation: "",
-            answer: null,
-            substitute : "",
-        };
-    },
-    computed: {
-    },
-    watch:  {
-    },
-    methods: {
-        num(x){
-            this.equation += x;
+	data() {
+		return {
+			equation: '',
+			answer: null,
+			substitute: '',
+			recentEquations: [],
+			recentAnswers: []
+		};
+	},
+	computed: {},
+	watch: {},
+	methods: {
+		num(x) {
+			this.equation += x;
+		},
+		operator(operator) {
+			this.equation += operator;
+		},
+		del() {
+			if (this.equation.substring(this.equation.length - 3) == 'mod')
+				this.equation = this.equation.substring(0, this.equation.length - 3);
+			else this.equation = this.equation.substring(0, this.equation.length - 1);
+		},
+		mod() {
+			this.equation += 'mod';
+		},
+		pie() {
+			this.equation += 'π';
+		},
+		sqrt() {
+			this.equation += '√';
+		},
+		pow2() {
+			this.equation += '²';
+		},
+		compute() {
+			let equation = this.equation;
 
-        },
-        operator(operator){
-            this.equation += operator;
+			equation = equation.replace(/²/g, '**2');
 
-        },
-        del(){
-            if ( this.equation.substring(this.equation.length-3) == 'mod')
-                this.equation = this.equation.substring(0,this.equation.length-3)
-            else
-                this.equation = this.equation.substring(0,this.equation.length-1)
-        },
-        mod(){
-            this.equation += 'mod';
-        },
-        pie(){
-            this.equation += 'π';
-        },
-        sqrt(){
-            this.equation += '√';
-        },
-        pow2(){
-            this.equation += '²';
-        },
-        compute(){
-            for(let i = 0; i < this.equation.length; i++){
-                if ( this.equation.substring(i,i + 3) == "mod"){
-                    this.substitute = this.equation.substring(0, i ) + "%" + this.equation.substring(i+3)
-                    this.answer = eval(this.substitute)
-                    this.equation = ""
-                    return null;
-                }
-                if ( this.equation.substring(i,i + 1) == "%"){
-                    this.substitute = this.equation.substring(0, i )/100
-                    this.answer = eval(this.substitute)
-                    this.equation = ""
-                    return null;
-                }
-                if ( this.equation.substring(i,i + 1) == "π"){
-                    this.substitute = this.equation.substring(0, i ) + "3.14159265358979323846264338327950288419716939937510" + this.equation.substring(i+1)
-                    this.answer = eval(this.substitute)
-                    this.equation = ""
-                    return null;
-                }
-                
-            }
-            try{
-                this.answer = eval(this.equation)
-                
-            }
-            catch{
-                alert("that didnt work xd")
-            }
-            this.equation = ""
-        },
+			equation = equation.replace(/π/g, '(Math.PI)');
+			equation = equation.replace(/√(\d+)/g, '(Math.sqrt($1))');
+			equation = equation.replace(/(\d+)\(/g, '$1*(');
+			equation = equation.replace(/(\d+)%/g, '($1/100)');
 
-    },
+			try {
+				this.answer = eval(equation);
+			} catch {
+				alert('An error occurred while evaluating the expression');
+				this.answer = 'ERROR';
+			}
+
+			if (this.recentEquations.length >= 2) {
+				this.recentEquations.splice(0, 1, this.recentEquations[1]);
+				this.recentAnswers.splice(0, 1, this.recentAnswers[1]);
+			}
+
+			this.recentEquations.splice(1, 1, this.equation);
+			this.recentAnswers.splice(1, 1, this.answer);
+			this.equation = '';
+			this.answer = '';
+		},
+		setEquationFromHistory(index) {
+			this.equation = this.recentEquations[index];
+		},
+		setEquationFromHistoryy(index) {
+			this.equation += this.recentAnswers[index];
+		}
+	}
 }).mount('#app');
